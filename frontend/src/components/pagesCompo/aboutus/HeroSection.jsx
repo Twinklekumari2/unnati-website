05606f9a320b63a7737d33fcc1d686844125164d
Twinklekumari2 <twinklekumari246@gmail.com>
2026-04-13 03:12:41 +0530

    fixes responsiveness 2

--- frontend/src/components/pagesCompo/aboutus/HeroSection.jsx
+++ frontend/src/components/pagesCompo/aboutus/HeroSection.jsx
@@ -5,10 +5,10 @@ import swachhtaPakhwara from './../../../assets/swachhtaPakhwara.jpg'
 
 const HeroSection = () => {
   return (
-    <section className=" text-white relative bg-cover bg-center bg-no-repeat flex justify-start pt-10 pb-10 xl:pb-52 px-10 xl:px-62 sm:py-30 sm:pb-51 items-center min-h-[50vh] h-screen" style={{backgroundImage: `url(${swachhtaPakhwara})`}}>
+    <section className=" text-white relative bg-cover bg-center bg-no-repeat flex justify-start py-30 xl:pb-52 px-10 xl:px-62 sm:py-30 sm:pb-51 items-center min-h-[50vh] h-screen" style={{backgroundImage: `url(${swachhtaPakhwara})`}}>
         <div className="absolute inset-0 bg-black/70"></div>
       <div className="flex z-10 flex-col justify-start gap-4">
-        <div className="oswald-font text-5xl tracking-wide">
+        <div className="oswald-font text-3xl sm:text-3xl md:text-5xl tracking-wide">
           <h1>EMPOWERING</h1>
           <h1>DREAMS</h1>
         </div>
