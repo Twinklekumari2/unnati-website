78d899641aae382eea7f0e6e0750c0b11ac096ef
zencoder06 <ankitgupta.odb09@gmail.com>
2026-04-14 17:15:10 +0530

    media minor change

--- frontend/src/components/pagesCompo/akshar/HeroSection.jsx
+++ frontend/src/components/pagesCompo/akshar/HeroSection.jsx
@@ -7,13 +7,13 @@ const HeroSection = () => {
     const navigate = useNavigate();
   return (
     <section
-      className="relative bg-cover bg-center bg-no-repeat flex justify-between items-center min-h-[50vh] h-screen text-white"
+      className="relative opacity-75 bg-cover bg-center bg-no-repeat flex justify-between items-center min-h-[50vh] h-screen text-white"
       style={{ backgroundImage: `url(${bgImg})` }}
     >
       <div className="absolute inset-0 z-10 bg-black/40"></div>
-      <div className="z-20 flex justify-between items-center w-full">
+      <div className="z-20 flex justify-between flex-col items-center w-full">
         <div className="flex justify-center items-center">
-          <div className="w-60 h-50 flex justify-center items-center">
+          <div className="w-60 h-50 flex justify-center items-center opacity:5000 saturate-700">
             <img src={Akshar} alt="" />
           </div>
           <div className="z-40">
@@ -26,7 +26,7 @@ const HeroSection = () => {
             </p>
           </div>
         </div>
-        <div className="mr-10 mt-10 flex flex-col gap-5">
+        <div className="mr-10 mt-10 flex flex-row gap-5">
           <div
             className="relative h-50 w-50 text-2xl flex justify-center items-center rounded-2xl bg-[#c3c3c3] border-8 border-white/35 text-black purple-purse-regular cursor-pointer bg-cover bg-center bg-no-repeat "
             style={{ backgroundImage: `url(${bgImg})` }}
--- frontend/src/components/pagesCompo/digiXplore/HeroSection.jsx
+++ frontend/src/components/pagesCompo/digiXplore/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   return (
     <section className="relative bg-cover bg-center bg-no-repeat flex justify-between items-center min-h-[50vh] h-screen text-white" style={{backgroundImage:`url(${bgImg})`}}>
        <div className="absolute inset-0 z-10 bg-black/40"></div>
-       <div className='z-20 flex justify-between items-center w-full'>
+       <div className='z-20 flex flex-col justify-between items-center w-full'>
         <div className='flex justify-center items-center'>
             <div className='w-60 h-50 flex justify-center items-center'>
                 <img src={digiXplore} alt="" />
@@ -16,7 +16,7 @@ const HeroSection = () => {
                 <p className='stick-no-bills tracking-wider'>Empowering Setudents With Computer Skills And Digital Confidence To Explore A Smarter Future.</p>
             </div>
         </div>
-        <div className='mr-10 mt-10 flex flex-col gap-5'>
+        <div className='mr-10 flex flex-row gap-5 justify-center'>
             <div className='h-50 w-50 text-2xl flex justify-center items-center rounded-2xl bg-[#c3c3c3] border-8 border-white/35 text-black purple-purse-regular cursor-pointer text-center'>
                 WORKSHOP <br /> 1
             </div>
@@ -25,6 +25,9 @@ const HeroSection = () => {
             </div>
             <div className='h-50 w-50 text-2xl flex justify-center items-center rounded-2xl bg-[#c3c3c3] border-8 border-white/35 text-black purple-purse-regular cursor-pointer text-center'>
                 WORKSHOP <br /> 3
+            </div>
+            <div className='h-50 w-50 text-2xl flex justify-center items-center rounded-2xl bg-[#c3c3c3] border-8 border-white/35 text-black purple-purse-regular cursor-pointer text-center'>
+                WORKSHOP <br /> 4
             </div>
         </div>
        </div>
